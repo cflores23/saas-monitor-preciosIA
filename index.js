9693b2977f79b8a32f425897083c3e64010513e2
Cam2375 <forca2375@gmail.com>
2025-09-25 23:30:40 -0600

    Primer commit: estructura inicial del proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor corriendo!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
