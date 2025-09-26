5b70de0653862e7ce1c3fae8be9ec26277604898
Cam2375 <forca2375@gmail.com>
2025-09-26 12:39:14 -0600

    Configuración de rutas y variables de entorno

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
-const express = require('express');
-require('dotenv').config();
-
 const app = require('./src/app');
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-});
+// Evitar imprimir varias veces si se usan clusters (PM2)
+const cluster = require('cluster');
+
+if (!cluster.isWorker) {
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  });
+} else {
+  // En workers solo levantamos el servidor, sin imprimir
+  app.listen(PORT);
+}
 
