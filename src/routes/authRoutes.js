f48ad442d7b35d307e56e49f55c71f4915e529f3
Cam2375 <forca2375@gmail.com>
2025-09-30 21:37:29 -0600

    Cambio para hacer login con pop up de google

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { OAuth2Client } = require('google-auth-library');
 const User = require('../models/User');
-const { ensureAuthenticated } = require('../middleware/authMiddleware');
+const { ensureAuthenticated } = require('../middleware/middleware');
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const client = new OAuth2Client(CLIENT_ID);
